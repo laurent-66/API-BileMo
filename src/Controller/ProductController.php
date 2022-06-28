7c7f50393a84b00e31c5bcf329abadb3942c3f73
Laurent LESAGE <56911526+laurent-66@users.noreply.github.com>
2022-06-28 19:21:42 +0200

    22-06-28 détails product done

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +13,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    #[Route('/api/products', name: 'allproducts', methods: ['GET'])]
-    public function getAllProducts(
-        ProductRepository $productRepository,
-        SerializerInterface $serializer
-    ): JsonResponse
+    #[Route('/api/products', name: 'allproducts', methods:['GET'])]
+    public function getAllProducts(ProductRepository $productRepository,SerializerInterface $serializer): JsonResponse
     {
         $productList = $productRepository->findAll();
         $jsonproductList = $serializer->serialize($productList, 'json', ['groups' => 'getproducts']);
         return new JsonResponse($jsonproductList, Response::HTTP_OK, [], true);
     }
+
+    #[Route('/api/products/{id}', name: 'detailproducts', methods:['GET'])]
+    public function getDetailProduct( int $id, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    {
+            $product = $productRepository->find($id);
+            $jsonProduct = $serializer->serialize($product, 'json', ['groups' => 'getproducts']);
+            return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
+    }
+
 }
